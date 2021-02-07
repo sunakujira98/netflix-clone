cd84f051592e84e19fcc124da2585fad8027290d
Steven Theodore <steventheodore98@gmail.com>
2021-02-07 22:55:39 +0700

    Faqs and accordion component done 2:00:13

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -12,11 +12,58 @@ export const Inner = styled.div`
   margin: auto;
 `
 
-export const Item = styled.div``
+export const Item = styled.div`
+  color: white;
+  margin-bottom: 10px;
+  max-width: 670px;
 
-export const Header = styled.div``
+  &:first-of-type {
+    margin-top: 3em;
+  }
+`
 
-export const Body = styled.div``
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
+  margin-bottom: 1px;
+  font-size: 26px;
+  font-weight: normal;
+  background: #303030;
+  padding: 0.8em 1.2em 0.8em 1.2em;
+  user-select: none;
+  align-items: center;
+
+  img {
+    filter: brightness(0) invert(1);
+    width: 24px;
+
+    @media (max-width: 600px) {
+      width: 16px;
+    }
+
+    @media (max-width: 600px) {
+      font-size: 16px;
+    }
+  }
+`
+
+export const Body = styled.div`
+  max-height: 1200px;
+  transition: max-height 0.25s cubic-bezier(0.5, 0, 0.1, 1);
+  font-size: 26px;
+  font-weight: normal;
+  line-height: normal;
+  background: #303030;
+  white-space: pre-wrap;
+  padding: 0.8em 2.2em;
+  user-select: none;
+
+  @media (max-width: 600px) {
+    font-size: 16px;
+    line-height: 22px;
+  }
+`
 
 export const Title = styled.h1`
   font-size: 50px;
