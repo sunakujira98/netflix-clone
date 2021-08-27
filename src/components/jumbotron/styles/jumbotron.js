7bcacea418eb0e36d9e1f3270ca28b1017bf5dd3
Steven Theodore <steventheodore98@gmail.com>
2021-08-28 01:10:57 +0700

    Starting footer component

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -23,7 +23,11 @@ export const Inner = styled.div`
 `
 
 export const Container = styled.div`
-
+  @media(max-width: 1000px) {
+    ${Item}:last-of-type h2 {
+      margin-bottom: 50px;
+    }
+  }
 `;
 
 export const Pane = styled.div`
