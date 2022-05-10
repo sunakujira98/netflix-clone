db7481f20df9db119846f524e5ea395784a2d0a8
Steven Theodore <steventheodore98@gmail.com>
2022-05-10 22:49:12 +0800

    Prepare for Browse container

--- src/app.js
+++ src/app.js
@@ -1,23 +1,39 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Home, Browse, Signin, Signup } from './pages'
 import * as ROUTES from './constants/routes'
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
+import { useAuthListener } from './hooks'
 
 export default function App() {
+  const { user } = useAuthListener()
+
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}>
-        <Home/>
-      </Route>
-      <Route exact path={ROUTES.BROWSE}>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+      >
+        <Signin />
+      </IsUserRedirect>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+      >
+        <Signup />
+      </IsUserRedirect>
+      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
         <Browse/>
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
-        <Signin/>
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
-        <Signup/>
-      </Route>
+      </ProtectedRoute>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.HOME}
+      >
+        <Home/>
+      </IsUserRedirect>
     </Router>
   )
 }
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import { BrowseContainer } from '../containers/browse'
+import { useContent } from '../hooks'
+import selectionFilter from '../utils/selection-filter'
 
 export default function Browse() {
-  return (<p>This is the Browse page</p>)
+  const { series } = useContent('series')
+  const { films } = useContent('films')
+
+  const slides = selectionFilter({ series, films })
+
+  return <BrowseContainer slides={slides} />
 }
