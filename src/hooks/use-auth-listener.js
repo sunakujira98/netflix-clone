4e135fda990accbf71bec7eb7237ab54a6e97ea5
Steven Theodore <steventheodore98@gmail.com>
2022-05-11 00:58:13 +0800

    Spinner done

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect, useContext } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { FirebaseContext } from '../context/firebase'
-
 
 export default function useAuthListener() {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')))
 
-  const  {firebase} = useContext(FirebaseContext)
+  const {firebase} = useContext(FirebaseContext)
   useEffect(() => {
     const listener = firebase.auth().onAuthStateChanged((authUser) => {
       if (authUser) {
